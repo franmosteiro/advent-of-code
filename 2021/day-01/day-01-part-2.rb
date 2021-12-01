5311a2bbac2e4b8d7f855314183f9104707151e2
Fran Mosteiro <fran.mosteiro@sequra.es>
2021-12-01 10:28:28 +0100

    day01 - part 2

---
+++ 2021/day-01/day-01-part-2.rb
@@ -0,0 +1,23 @@
+class Day01Part1
+    
+    def initialize(input)
+        @input_contents = IO.read(input)
+    end
+
+    def part02
+
+        @input_contents
+            .lines
+            .map(&:to_i)
+            .each_cons(3)
+            .map(&:sum)
+            .each_cons(2)
+            .count { |first, second| 
+                first < second 
+        }
+
+    end
+
+end
+
+p Day01Part1.new("./day-01-input.txt").part02
