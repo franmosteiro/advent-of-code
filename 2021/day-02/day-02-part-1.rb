409f4b452229649ead107b55b3d2d5ea7d751a7f
Fran Mosteiro <fran.mosteiro@sequra.es>
2021-12-02 10:34:30 +0100

    Refactor day2 solutions

--- 2021/day-02/day-02-part-1.rb
+++ 2021/day-02/day-02-part-1.rb
@@ -3,32 +3,30 @@ require "../file_helper.rb"
 class Day02Part1
     
     attr_reader :input_contents
+    attr_accessor :horizontal, :depth
 
     def initialize(input)
         @input_contents = FileHelper.parse(input)
+        @horizontal = 0
+        @depth = 0
     end
 
     def part01
-
-        horizontal = 0
-        depth = 0
-
-        input_contents
-            .map{ |line|
-                direction, steps = line.split(" ")
-
-                case direction 
-                when "forward"
-                    horizontal += steps.to_i
-                when "up"
-                    depth -= steps.to_i
-                when "down"
-                    depth += steps.to_i
-                end
-            }
-
+        input_contents.map{ |direction_line| parse_direction(direction_line)}
         horizontal * depth
+    end
 
+    private
+    def parse_direction(direction_line)
+        direction, steps = direction_line.split(" ")
+        case direction 
+        when "forward"
+            @horizontal += steps.to_i
+        when "up"
+            @depth -= steps.to_i
+        when "down"
+            @depth += steps.to_i
+        end
     end
 
 end
--- 2021/day-02/day-02-part-2.rb
+++ 2021/day-02/day-02-part-2.rb
@@ -3,36 +3,33 @@ require "../file_helper.rb"
 class Day02Part2
     
     attr_reader :input_contents
+    attr_accessor :horizontal, :depth, :aim
 
     def initialize(input)
         @input_contents = FileHelper.parse(input)
+        @horizontal = 0
+        @depth = 0
+        @aim = 0
     end
 
     def part02
-
-        horizontal = 0
-        depth = 0
-        aim = 0
-
-        input_contents
-            .map{ |line|
-                direction, steps = line.split(" ")
-
-                case direction 
-                when "forward"
-                    horizontal += steps.to_i
-                    if aim > 0
-                        depth += steps.to_i * aim
-                    end
-                when "up"
-                    aim -= steps.to_i
-                when "down"
-                    aim += steps.to_i
-                end
-            }
-            
+        input_contents.map{ |direction_line| parse_direction(direction_line)}
         horizontal * depth
+    end
 
+    def parse_direction(direction_line)
+        direction, steps = direction_line.split(" ")
+        case direction 
+        when "forward"
+            @horizontal += steps.to_i
+            if @aim > 0
+                @depth += steps.to_i * aim
+            end
+        when "up"
+            @aim -= steps.to_i
+        when "down"
+            @aim += steps.to_i
+        end
     end
 
 end
